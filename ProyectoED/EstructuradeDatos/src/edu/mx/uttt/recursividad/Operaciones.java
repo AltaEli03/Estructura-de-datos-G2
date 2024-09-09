6e02bb8623daa3d287d8f5268dcd1224969cc6d4
Eliuht Altamirano Granados <23300043@uttt.edu.mx>
2024-09-09 14:54:08 -0600

    Logro: Creacion de menu y programa de "hola mundo" recursivo

--- ProyectoED/EstructuradeDatos/src/edu/mx/uttt/recursividad/Operaciones.java
+++ ProyectoED/EstructuradeDatos/src/edu/mx/uttt/recursividad/Operaciones.java
@@ -1,5 +1,40 @@
 package edu.mx.uttt.recursividad;
 
 public class Operaciones {
+    private int n;
 
+    public Operaciones() {
+        this.n = 1;
+    }
+
+    public int getN(int n) {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = (n > 0) ? n : 1;
+    }
+
+    // metodo iterativo
+
+    public void imprimir() {
+        int i = 1;
+        while (i <= this.n) {
+            System.out.println("Hola mundo");
+            i++;
+        }
+    }
+
+    // metodo recursivo
+    public void imprimir(int n) {
+        // Caso Base
+        if (n == 1) {
+            System.out.println("Hola mundo");
+            ;
+        } else {
+            // Caso general
+            System.out.println("Hola mundo");
+            imprimir(n - 1);
+        }
+    }
 }
--- ProyectoED/EstructuradeDatos/src/edu/mx/uttt/recursividad/testRecursividad.java
+++ ProyectoED/EstructuradeDatos/src/edu/mx/uttt/recursividad/testRecursividad.java
@@ -1,7 +1,48 @@
 package edu.mx.uttt.recursividad;
 
+import javax.swing.JOptionPane;
+
 public class testRecursividad {
     public static void main(String[] args) {
-        
+        ejecutar();
     }
+
+    public static String menu() {
+        String menu = "Menu Principal\n" +
+                "1) Metodo Iterativo\n" +
+                "2) Metodo Recursivo\n" +
+                "3) Salir\n" +
+                "Elige la opcion";
+        return JOptionPane.showInputDialog(menu);
+    }
+
+    public static void ejecutar() {
+        boolean sentinel = true;
+        String opcion = "";
+        Operaciones op = new Operaciones();
+        int n = 0;
+
+        do {// Empieza el do-while
+            opcion = menu();
+            switch (opcion) {
+                case "1":
+                    n = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de veces a imprimir"));
+                    op.setN(n);
+                    op.imprimir();
+                    break;
+                case "2":
+                    n = Integer.parseInt(JOptionPane.showInputDialog("Introduce el numero de veces a imprimir"));
+                    op.imprimir(op.getN(n));
+                    break;
+                case "3":// Salir
+                    sentinel = false;
+                    JOptionPane.showMessageDialog(null, "Hasta la proxima");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Valor no valido");
+                    break;
+            }// Cierra el switch
+        } while (sentinel);// Cierra el do-while
+
+    }// Cierra el metodo
 }
